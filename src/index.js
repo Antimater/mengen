0f37c0073ca229540f99e4b27b88ca9b76d2be12
Antimater <marceljung97@hotmail.de>
2023-06-07 18:24:44 +0200

    firt json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 let elementSubmit = document.getElementById("sendValues");
 elementSubmit.addEventListener("click", submitData);
+
 function submitData() {
+    let url = "https://github.com/Antimater/mengen/blob/master/data/data.json";
+
     let selectedProduct = document.getElementById("selectProduct").value;
     let inputAmount = document.getElementById("inputMenge").value;
     let inputPassword = document.getElementById("inputPassword").value;
+
     console.log(selectedProduct);
     console.log(inputAmount);
     console.log(inputPassword);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            // Do stuff with the contents of the JSON file here
+        })
 }
